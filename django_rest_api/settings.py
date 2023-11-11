4febf4943fd5eced9d06b6aeda2b75360671a645
srikanth-selvaaraj <srikanth.selvaaraj@gmail.com>
2023-11-11 21:41:11 +0700

    python decouple correctly configured.

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -1,11 +1,9 @@
 from pathlib import Path
-from decouple import Config, Csv
-
-config = Config()
+from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -108,7 +106,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.c;om/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
