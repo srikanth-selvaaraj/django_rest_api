a36282cafd7d7769fa8efefcb7c90d2ac34a1201
srikanth-selvaaraj <srikanth.selvaaraj@gmail.com>
2023-11-13 23:34:41 +0700

    all logics moved to class based

--- crud_api/urls.py
+++ crud_api/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list_blogs', views.list_blogs, name='list_blogs'),
-    path('get_blog/<int:pk>', views.get_blog, name='get_blog'),
-    path('create_blog', views.create_blog, name='create_blog'),
-    path('update_blog/<int:pk>', views.update_blog, name='update_blog'),
-    path('delete_blog/<int:pk>', views.delete_blog, name='delete_blog'),
+    path('blogs', views.BlogList.as_view(), name='blogs'),
+    path('blogs/<int:pk>', views.BlogActions.as_view(), name='blog_actions'),
 ]
--- crud_api/views.py
+++ crud_api/views.py
@@ -1,78 +1,107 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import BlogSerializer
 from .models import Blog
-from json import load
 
 
-@api_view(['GET'])
-def list_blogs(request):
-    try:
-        blogs = Blog.objects.all()
-        if blogs:
-            serialized_blogs = BlogSerializer(blogs, many=True)
-            return Response({'response': serialized_blogs.data}, status=status.HTTP_200_OK)
-        else:
-            return Response({'response': 'No blogs Found'}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        # log e
-        return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class BlogList(APIView):
+    """
+    List all the blogs or create a new one
+    """
+
+    def get(self, request, format=None):
+        try:
+            blogs = Blog.objects.all()
+            if blogs:
+                serialized_blogs = BlogSerializer(blogs, many=True)
+                return Response({'response': serialized_blogs.data}, status=status.HTTP_200_OK)
+            else:
+                return Response({'response': 'No blogs Found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            # log e
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request, format=None):
+        try:
+            blog = BlogSerializer(data=request.data)
+            if blog.is_valid():
+                blog.save()
+                return Response({'response': 'Blog created successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'response': blog.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # log e
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['POST'])
-def create_blog(request):
-    try:
-        blog = BlogSerializer(data=request.data)
-        if blog.is_valid():
-            blog.save()
-            return Response({'response': 'Blog created successfully'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'response': blog.errors}, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as e:
-        # log e
-        return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class BlogActions(APIView):
+    """
+    Return a single blog, update and delete blog
+    """
 
+    def get_blog(self, pk):
+        return Blog.objects.get(pk=pk)
 
-@api_view(['GET'])
-def get_blog(reqeust, pk):
-    try:
-        blog = Blog.objects.get(pk=pk)
-        serialized_blog = BlogSerializer(blog, many=False)
-        return Response({'response': serialized_blog.data}, status=status.HTTP_200_OK)
-    except Blog.DoesNotExist:
-        return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return (Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR))
+    def get(self, request, pk):
+        try:
+            blog = self.get_blog(pk)
+            serialized_blog = BlogSerializer(blog, many=False)
+            return Response({'response': serialized_blog.data}, status=status.HTTP_200_OK)
+        except Blog.DoesNotExist:
+            return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    def post(self, request):
+        try:
+            blog = BlogSerializer(data=request.data)
+            if blog.is_valid():
+                blog.save()
+                return Response({'response': 'Blog created successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'response': blog.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # log e
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-@api_view(['PUT', 'PATCH'])
-def update_blog(request, pk):
-    try:
-        blog = Blog.objects.get(pk=pk)
-        if request.method == 'PUT':
+    def put(self, request, pk):
+        try:
+            blog = self.get_blog(pk)
             updated_blog = BlogSerializer(instance=blog, data=request.data)
-        else:
+
+            if updated_blog.is_valid():
+                updated_blog.save()
+                return Response({'response': 'Blog updated successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'response': updated_blog.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Blog.DoesNotExist:
+            return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def patch(self, request, pk):
+        try:
+            blog = self.get_blog(pk)
             updated_blog = BlogSerializer(instance=blog, data=request.data, partial=True)
 
-        if updated_blog.is_valid():
-            updated_blog.save()
-            return Response({'response': 'Blog updated successfully'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'response': updated_blog.errors}, status=status.HTTP_400_BAD_REQUEST)
-    except Blog.DoesNotExist:
-        return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return (Response({'response': f'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR))
+            if updated_blog.is_valid():
+                updated_blog.save()
+                return Response({'response': 'Blog updated successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'response': updated_blog.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Blog.DoesNotExist:
+            return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'response': 'Something Went Wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
-@api_view(['DELETE'])
-def delete_blog(request, pk):
-    try:
-        blog = Blog.objects.get(pk=pk)
-        blog.delete()
-        return Response({'response': 'Blog updated successfully'}, status=status.HTTP_200_OK)
-    except Blog.DoesNotExist:
-        return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return Response({'response': 'Something Went Wrong '}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    def delete(self, request, pk):
+        try:
+            blog = self.get_blog(pk)
+            blog.delete()
+            return Response({'response': 'Blog deleted successfully'}, status=status.HTTP_200_OK)
+        except Blog.DoesNotExist:
+            return Response({'response': 'Blog not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'response': 'Something Went Wrong '}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
